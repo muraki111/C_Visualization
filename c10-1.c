04bc390a566d5deee3dc0c60ff48bf4fc3017279
muraki_111 <60808536+muraki111@users.noreply.github.com>
2020-06-24 01:13:55 +0900

    サークル当たり判定用キューブ追加

--- c10-1.c
+++ c10-1.c
@@ -82,10 +82,22 @@ void display(void)
 		glPopMatrix();
 
 		glPushMatrix();//サークル
-		glRotatef(angle, 1, 0, 0);//サークル回転
+		glTranslatef(0, 0, 0);
+		glRotatef(angle, 1, 0, 0);					//サークル回転
 		glTranslatef(circle_x, circle_y + 1, circle_z); //サークル位置
-		glColor3f(1.0, 0, 0);//サークル色
+		glColor3f(1.0, 0, 0);			   //サークル色
 		glutWireTorus(0.05, 0.25, 50, 100);//サークル(太さ,大きさ,?,?)
+
+		//当たり判定用キューブ下
+		glColor3f(0, 0, 1.0);
+		glTranslatef(0,0-0.25,0);
+		glutWireCube(0.08);
+
+		//当たり判定用キューブ下
+		glColor3f(0, 0, 1.0);
+		glTranslatef(0,0 + 0.5,0);
+		glutWireCube(0.08);
+
 		glPopMatrix();
 
 	glPopMatrix();
@@ -126,7 +138,6 @@ void myMotion(int x, int y)
         elevation -= (float) yDisp/2.0;
         break;
     case GLUT_MIDDLE_BUTTON://回転
-		circle_x += 0.001;
         //twist = fmod (twist + xDisp, 360.0);
         break;
     case GLUT_RIGHT_BUTTON://拡大縮小
